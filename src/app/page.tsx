b1db04831532d960c2a02671971ab61f4ee30379
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-07 14:00:46 +0200

    this kinda works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,286 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import { useIntersectionObserver } from "usehooks-ts";
+
+type Tracks = SpotifyApi.RecommendationTrackObject[];
 
 export default function Home() {
+  const router = useRouter();
+  const [trackInput, setTrackInput] = useState<string>("");
+  const [mounted, setMounted] = useState<boolean>(false);
+  const token = Cookies.get("token");
+  const [recommendations, setRecommendations] = useState<Tracks>([]);
+  const [trackName, setTrackName] = useState<string>("hi");
+  const { isIntersecting, ref } = useIntersectionObserver({
+    threshold: 0.5,
+  });
+
+  console.log(isIntersecting);
+  // if (!token) {
+  //   throw new Error("No token found");
+  // }
+  // console.log("hi", token);
+  const isLoggedIn = token !== undefined && token !== "";
+  const player = useRef<Spotify.Player>();
+  useEffect(() => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      player.current = new Spotify.Player({
+        name: "Web Playback SDK Quick Start Player",
+        getOAuthToken: (cb) => {
+          cb(token as string);
+        },
+        volume: 0.5,
+      });
+
+      player.current.setName("field").then(() => {
+        console.log("field");
+      });
+
+      player.current.connect();
+
+      // Ready
+      player.current.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      // Not Ready
+      player.current.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+      player.current.addListener("initialization_error", ({ message }) => {
+        console.error(message);
+      });
+
+      player.current.addListener("authentication_error", ({ message }) => {
+        console.error(message);
+      });
+
+      player.current.addListener("account_error", ({ message }) => {
+        console.error(message);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!mounted) {
+      setMounted(true);
+    }
+  }, []);
+
+  console.log(recommendations.length);
+  useEffect(() => {
+    if (!isIntersecting) return;
+    console.log("hi");
+    if (recommendations.length >= 1000) return;
+    refetch();
+    setRecommendations([...recommendations, ...data.tracks]);
+  }, [isIntersecting]);
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["tracks"],
+    queryFn: async () => {
+      const data = await fetchRecs(getURI(trackInput));
+      return data;
+    },
+    enabled: false,
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  });
+
+  const last = recommendations ? recommendations.at(-1)?.id : null;
+  console.log(last);
+
+  async function fetchRecs(uri: string) {
+    const res = await axios.get("https://api.spotify.com/v1/recommendations", {
+      params: {
+        seed_tracks: uri,
+        seed_genres: ["pop", "rock", "indie"],
+        limit: 100,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!res.data) return;
+    setRecommendations(res.data.tracks);
+    return res.data;
+  }
+
+  function addToPlayback(uri: string) {
+    axios.post(
+      `https://api.spotify.com/v1/me/player/queue?uri=spotify:track:${encodeURIComponent(uri)}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+  }
+
+  function playTrack(uri: string) {
+    axios.put(
+      `https://api.spotify.com/v1/me/player/play`,
+      {
+        uris: [`spotify:track:${encodeURIComponent(uri)}`],
+        position_ms: 0,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+  }
+
+  function togglePlay(action: string) {
+    if (action === "play") {
+      console.log(player.current);
+      player.current?.togglePlay();
+    }
+    if (action === "next") {
+      console.log(player.current);
+      player.current?.nextTrack();
+    }
+    if (action === "seek") {
+      console.log(player.current);
+      player.current?.seek(25000);
+    }
+  }
+
+  async function handleInput(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!trackInput) {
+      return;
+    }
+
+    const trackURL = new URL(trackInput);
+    if (trackURL.hostname !== "open.spotify.com") {
+      throw new Error("Not a spotify url");
+    }
+    const uri = trackURL.pathname.split("/track/")[1];
+    refetch();
+  }
+
+  function getURI(url: string) {
+    const trackURL = new URL(trackInput);
+    if (trackURL.hostname !== "open.spotify.com") {
+      throw new Error("Not a spotify url");
+    }
+    const uri = trackURL.pathname.split("/track/")[1];
+    return uri;
+  }
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="flex justify-between">
+        {isLoggedIn ? (
+          <>
+            <form
+              onSubmit={handleInput}
+              className="flex-grow"
+            >
+              <input
+                type="text"
+                value={trackInput}
+                onChange={(e) => setTrackInput(e.target.value)}
+                placeholder="Enter spotify track url"
+                className="bg-slate-500 w-full placeholder:text-white placeholder:pl-2 rounded-sm"
+              ></input>
+            </form>
+            <button
+              onClick={() => {
+                Cookies.remove("token");
+                router.refresh();
+              }}
+              className="hover:bg-blue-300 mx-2 font-mono py-1 px-2"
+            >
+              log out
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="flex-grow bg-slate-500">
+              <p>log into spotify</p>
+            </div>
+            <a
+              href="api/auth/login"
+              className="bg-red-500 hover:bg-blue-300"
+            >
+              log in
+            </a>
+          </>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <p>{trackName}</p>
       </div>
+      <div className="grid lg:grid-cols-8 sm:grid-cols-6 grid-cols-4 w-full pb-12">
+        {/* <p>{JSON.stringify(tracks)}</p>
+         */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {recommendations
+          ? recommendations.map(
+              (t: SpotifyApi.RecommendationTrackObject, index) => (
+                <div
+                  onMouseOver={() => setTrackName(t.name)}
+                  onClick={() => {
+                    // router.push(`/track/${t["id"]}`);
+                    playTrack(t["id"]);
+                    togglePlay("next");
+                  }}
+                  key={`${t.id}`}
+                  ref={t.id === last ? ref : null}
+                >
+                  <div>
+                    <Image
+                      src={t.album.images[0]?.url}
+                      alt=""
+                      width={300}
+                      height={300}
+                    />
+                  </div>
+                </div>
+              ),
+            )
+          : null}
       </div>
-    </main>
-  )
+      <div className="bg-slate-500 fixed bottom-0 w-full flex items-center justify-center">
+        <button
+          className="hover:bg-blue-300 rounded-sm font-mono px-2 py-1 m-2"
+          onClick={() => togglePlay("previous")}
+        >
+          PRS
+        </button>
+        <button
+          className="hover:bg-blue-300 rounded-sm font-mono px-2 py-1 m-2"
+          onClick={() => togglePlay("play")}
+        >
+          PY
+        </button>
+        <button
+          className="hover:bg-blue-300 rounded-sm font-mono px-2 py-1 m-2"
+          onClick={() => togglePlay("next")}
+        >
+          NXT
+        </button>
+        <button
+          className="hover:bg-blue-300 rounded-sm font-mono px-2 py-1 m-2"
+          onClick={() => togglePlay("seek")}
+        >
+          seek
+        </button>
+      </div>
+    </>
+  );
 }
